9dd71645f7fc4609d2ea14e80d5fa08292aa285c
s.jcqs <satyan.jacquens@fabernovel.com>
2020-12-03 16:49:29 +0100

    Change

--- slack_leaderboard/functions/src/config.ts
+++ slack_leaderboard/functions/src/config.ts
@@ -7,9 +7,9 @@ const SLACK_KEY = "slack"
 const config = functions.config()
 
 export function adventOfCode(): AdventOfCodeConfig {
-    const aocConfig = config[ADVENT_OF_CODE_KEY]
-    const session = aocConfig.session
-    const url = aocConfig.url + ".json"
+    const adventOfCodeConfig = config[ADVENT_OF_CODE_KEY]
+    const session = adventOfCodeConfig.session
+    const url = adventOfCodeConfig.url + ".json"
 
     return { url: url, sessionValue: session }
 }
